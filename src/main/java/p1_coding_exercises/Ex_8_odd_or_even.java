05a1a52e116cb67eea856d0b835c2c08d3a524cc
MasterSpam <moritz@feuchter.email>
2023-07-29 12:52:36 +0200

    one more chapter: branches done!

--- src/main/java/p1_coding_exercises/Ex_8_odd_or_even.java
+++ src/main/java/p1_coding_exercises/Ex_8_odd_or_even.java
@@ -3,6 +3,7 @@ package p1_coding_exercises;
 public class Ex_8_odd_or_even {
 
     public static void main(String[] args) {
+
         // Die Variable int input existiert bereits.
         // Schreibe ein Programm, welches bei einer geraden Zahl das Wort "gerade" ausgibt,
         // bzw. bei einer ungeraden Zahl das Wort "ungerade".
